ccaf1a283bf184155a96b9e9ceb81ba9c527aece
Aryan-Kumar-Thakur <thakuraryan7678@gmail.com>
2023-10-26 15:03:36 +0530

    initial commit

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.mongo_url).then(()=>{
+mongoose.connect(process.env.mongo_url,{
+    dbName: "Todo_list"
+}).then(()=>{
     console.log("database connected successfully")
 }).catch((err)=>{
     console.log(err);
